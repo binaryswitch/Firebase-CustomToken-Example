a157e6905de8733103674a4fee8572b655e56ed8
Justin White <justinwhite@Justins-Mac-mini.local>
2016-09-19 19:55:26 +1200

    Basic working example.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-"" 
+//This is a project for generating never expiring custom firebase tokens.
+
+var firebase = require("firebase");
+
+//Download from Permissions -> Service Accounts -> Create Key
+var serviceJSONFile = "<file>.json"
+
+//Your database url
+var databaseUrl = "https://<server-name>.firebaseio.com/"
+
+//Reference service admin UUID. Authenticate as admin user.
+// **Needs Firebase admin rules change for use.
+var serviceUUID = "my-private-custom-service-uud"
+
+firebase.initializeApp({
+  serviceAccount: serviceJSONFile,
+  databaseURL: databaseUrl
+});
+
+var customToken = firebase.auth().createCustomToken(serviceUUID);
+
+console.log (customToken);
